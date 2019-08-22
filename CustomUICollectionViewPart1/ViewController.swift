25202a2a766bea2374083f02fd3dc5514233d88d
mohamedosama2074 <iosengks74@gmail.com>
2019-08-22 19:24:59 +0200

    Initial Commit

---
+++ CustomUICollectionViewPart1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomUICollectionViewPart1
+//
+//  Created by Mohamed on 8/22/19.
+//  Copyright © 2019 Mohamed74. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
